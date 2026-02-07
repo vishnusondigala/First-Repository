ebb76c2ca535c6fe26a29f4a2b28bbfdcdaca367
vishnusondigala <sondigalavishnu19@gmail.com>
2026-02-08 00:54:02 +0530

    In the abcd  app, models are created in models.py and registered in admin.py

--- abcd/admin.py
+++ abcd/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User,UserProfile
+from .models import User,UserProfile,Donor,Receiver
 
 # Register your models here.
 
 
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(Donor)
+admin.site.register(Receiver)
--- abcd/models.py
+++ abcd/models.py
@@ -42,3 +42,46 @@ class UserProfile(models.Model):
 
 #------------------------------------------------------------------------------------------------------------
 
+class Donor(models.Model):
+
+    DONOR_TYPE = (
+        ('Individual', 'Individual'),
+        ('Organization', 'Organization'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    donor_type = models.CharField(max_length=20, choices=DONOR_TYPE)
+    organization_name = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "donors"
+
+    def __str__(self):
+        return self.user.name
+    
+#----------------------------------------------------------------------------------------------------
+
+
+class Receiver(models.Model):
+
+    RECEIVER_TYPE = (
+        ('NGO', 'NGO'),
+        ('Shelter', 'Shelter'),
+        ('Orphanage', 'Orphanage'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization_name = models.CharField(max_length=150)
+    receiver_type = models.CharField(max_length=20, choices=RECEIVER_TYPE)
+    registration_no = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "receivers"
+
+    def __str__(self):
+        return self.organization_name
+
